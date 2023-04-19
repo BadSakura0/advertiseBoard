cd55c2d273c578e7a7bb7af996b945937b0cbbd1
BadSakura0 <feelsbadsakura@gmail.com>
2023-04-19 20:44:39 +0300

    Initial commit

---
+++ src/main/java/com/example/advertiseboard/AdvertiseBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.advertiseboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertiseBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvertiseBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/advertiseboard/AdvertiseBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advertiseboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertiseBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
